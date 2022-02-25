21364be305d9ccd00b0548fcaf00f843fa92c792
Martin Reichardt <mrlogin@mailbox.org>
2022-02-25 10:50:40 +0100

    Changed the script with my own API

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'http://localhost:8000/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
-  taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
+  taxiFareApiUrl = 'https://martinsamazingapi-4hqdf75jgq-ew.a.run.app/predict';
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
